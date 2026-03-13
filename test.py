2753542c412e76c4685e36efe767c78f62dbb5dc
Yang-Mini <leeyang1991@gmail.com>
2026-03-13 11:37:33 -0400

    rename

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
+import time
 
-print('running_python')
-import sys
+from rich.progress import Progress
 
-from lytools import *
-from pprint import pprint
-print(sys.path[4])
-sleep(30)
-print('Done')
+with Progress() as progress:
+
+    task1 = progress.add_task("[red]Downloading...", total=100)
+    task2 = progress.add_task("[green]Processing...", total=50)
+    task3 = progress.add_task("[cyan]Cooking...", total=200)
+
+    while not progress.finished:
+        progress.update(task1, advance=0.5)
+        progress.update(task2, advance=0.5)
+        progress.update(task3, advance=0.5)
+        time.sleep(0.02)
